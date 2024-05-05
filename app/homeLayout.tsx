c38104393eb6963a5e9d4fe1f7914652711abfb4
OnlyKnox <joshua.knox927@gmail.com>
2024-05-05 00:56:06 -0400

    small changes and file tree setup

---
+++ app/homeLayout.tsx
@@ -0,0 +1,10 @@
+﻿export function HomeLayout() {
+  return <h1>Put components that design the landing page here...</h1>
+  // <Hero />
+  // <Information />
+  // <Skills />
+  // <Tidbits />
+  // <Projects />
+  // <Gallery />
+  // <Contact />
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { HomeLayout } from '@/app/homeLayout'
+
 export default function Home() {
   return (
     <main>
       <h1>Hello Port</h1>
+      <HomeLayout />
     </main>
   )
 }
